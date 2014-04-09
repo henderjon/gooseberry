9290e9a80eee9e8570eb9b4502d81786b4cb5371
henderjon <jhenderson@capstonedigital.com>
2014-04-09 13:28:01 -0500

    proof of concept

--- examples/example.php
+++ examples/example.php
@@ -16,6 +16,12 @@ $endpoints = array(
 $dingle = new \Gooseberry\Gooseberry($api, $endpoints);
 
 //$var = $Obj->HTTPMethod($endoint, $queryData);
+
+$dingle->setHeaders([
+	"X-test-header" => "this is totally a test header",
+	"User-Agent"    => "does this work?",
+]);
+
 $response = $dingle->post("post", array("test_key" => "test_value"));
 
 print_r($response);
