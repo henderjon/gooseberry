e5fe638495f0cdc4e863d7930085a8cb91f8cc9b
henderjon <jhenderson@capstonedigital.com>
2014-04-09 13:28:01 -0500

    proof of concept

--- src/Gooseberry.php
+++ src/Gooseberry.php
@@ -15,6 +15,16 @@ class Gooseberry extends cURLOptions {
 	 * hold the cURL connection info
 	 */
 	protected $responseInfo = array();
+
+	protected $method, $user_agent;
+
+	protected $headers = array();
+
+	function setHeaders(array $headers){
+		foreach($headers as $header => $value){
+			$this->headers[$header] = trim($value);
+		}
+	}
 	/**
 	 * Description
 	 * @param type $user
@@ -25,70 +35,80 @@ class Gooseberry extends cURLOptions {
 		$this->apiURL    = $api;
 		$this->endpoints = $endpoints;
 	}
-	/**
-	 * method to set te auth headers
-	 * @param string $user Username
-	 * @param string $pass Password
-	 * @return
-	 */
-	function setAuth($user, $pass = "", $method = CURLAUTH_BASIC){
-		$this->setAdditionalOptions(array(
-			CURLOPT_USERPWD => sprintf("%s:%s", $user, $pass),
-			CURLOPT_HTTPAUTH => $method,
-		));
-	}
 	/***/
 	function get($endpoint, array $data = array()){
-		$url = $this->normalizeEndpoint($endpoint);
-		if($url){
-			$this->setGetRequest($url, $data);
-			return $this->ping();
-		}
-		throw new Exceptions\InvalidEndpointException("That endpoint doesn't exist");
+		$url = $this->normalizeEndpoint($endpoint, $data);
+
+		$context = $this->createContext("GET");
+
+		return $this->ping($url, $context);
 	}
 	/***/
 	function post($endpoint, array $data = array()){
 		$url = $this->normalizeEndpoint($endpoint);
-		if($url){
-			$this->setPostRequest($url, $data);
-			return $this->ping();
+
+		$this->setHeaders(["Content-type" => "application/x-www-form-urlencoded"]);
+
+		$context = $this->createContext("POST", $data);
+
+		return $this->ping($url, $context);
+	}
+	/***/
+	protected function createContext($method, array $data = array()){
+
+		$opts = array("http" => array("method" => $method));
+
+		if($this->headers){
+			$opts["http"]["header"] = $this->assembleHeaders();
 		}
-		throw new Exceptions\InvalidEndpointException("That endpoint doesn't exist");
-	}
-	/***/
-	function put($endpoint, array $data = array()){
-		$url = $this->normalizeEndpoint($endpoint);
-		if($url){
-			$this->setPutRequest($url, $data);
-			return $this->ping();
+
+		if($this->user_agent){
+			$opts["http"]["user_agent"] = $this->user_agent;
 		}
-		throw new Exceptions\InvalidEndpointException("That endpoint doesn't exist");
+
+		if($data){
+			$opts["http"]["content"] = $this->toQueryString($data);
+		}
+
+		return stream_context_create($opts);
 	}
 	/***/
-	function getInfo($opt = 0){
-		return curl_getinfo($this->handle, $opt);
-	}
-	/***/
-	protected function normalizeEndpoint($endpoint){
+	protected function normalizeEndpoint($endpoint, array $data = array()){
 		$url = "";
-		// drop($endpoint, $this->endpoints);
+
 		if(in_array($endpoint, $this->endpoints)){
 			$url = "{$this->apiURL}/{$endpoint}";
+			if($data){
+				$url = sprintf("%s?%s", rtrim($url, " ?"), $this->toQueryString($data));
+			}
+		}else{
+			throw new Exceptions\InvalidEndpointException("That endpoint doesn't exist");
 		}
+
 		return $url;
 	}
-	/**
-	 * magic method catchall
-	 * @param string $name The name of the endpoint as a method call
-	 * @param array $args An array of parameters
-	 * @return object
-	 */
-	protected function ping(){
-		$handle = curl_init();
-		curl_setopt_array($handle, $this->options);
-		$response           = curl_exec($handle);
-		$this->responseInfo = curl_getinfo($handle);
-		curl_close($handle);
+	/***/
+	protected function toQueryString(array $data = array()){
+		if($data){
+			return http_build_query($data, "no_", "&");
+		}
+	}
+	/***/
+	protected function assembleHeaders(){
+		$finalHeader = array();
+		foreach($this->headers as $header => $value){
+			$finalHeader[] = "{$header}: {$value}";
+		}
+		return implode("\r\n", $finalHeader);
+	}
+	/***/
+	protected function ping($url, $context){
+		$stream = fopen($url, 'r', false, $context);
+		$this->responseInfo = stream_get_meta_data($stream);
+		$response = stream_get_contents($stream);
+		fclose($stream);
 		return $response;
 	}
 }
+
+// http://www.daniweb.com/web-development/php/threads/370401/multiple-file-upload-problem-using-stream_context_create-function
